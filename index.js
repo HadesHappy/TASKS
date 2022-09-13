0e3a6505416b2365d0bb93e227eb876fdb018b22
HadesHappy <happyhades123@gmail.com>
2022-09-13 13:42:56 +0900

    restful api completed

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 let express = require('express');
+let morgan = require('morgan');
+let rt = require("file-stream-rotator");
+let Writable = require("stream").Writable
 let mongoose = require('mongoose');
 let cors = require('cors');
 let createError = require('http-errors');
 let bodyParser = require('body-parser');
+
+class TerminalStream extends Writable {
+    write(line) {
+        // Here you send the log line to wherever you need
+        console.log("Logger: ", line);
+    }
+}
+let fileWriter = rt.getStream({
+    filename: "tasks.log",
+    frequency: "daily",
+    verbose: true
+});
+let terminalWriter = new TerminalStream();
+
+// Skip requests that aren't for the homepage.
+let skipSuccess = (req, res) => res.statusCode < 400;
+let skipError = (req, res) => res.statusCode >= 400;
 // Express Route
 const taskRoute = require('./routes/task.route');
-const { MongoCursorExhaustedError } = require('mongodb');
 // Connecting mongoDB Database
 mongoose
     .connect('mongodb://127.0.0.1:27017/mydatabase')
@@ -21,6 +40,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cors());
+// Success logging
+app.use(morgan('combined', {
+    skip: skipError,
+    stream: fileWriter
+}))
+
+// Error logging
+app.use(morgan('combined', {
+    skip: skipSuccess,
+    stream: terminalWriter
+}))
 
 app.use('/tasks', taskRoute);
 
